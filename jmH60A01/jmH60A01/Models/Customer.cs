790b5bbf5f82f086176bd4a6c46f8abe17e3f634
JMorency <jvmorency@gmail.com>
2024-09-10 21:29:15 -0400

    Add Part B Models

---
+++ jmH60A01/jmH60A01/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace jmH60A01.Models;
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public string Province { get; set; }
+    
+    public string CreditCard { get; set; }
+}
---
+++ jmH60A01/jmH60A01/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace jmH60A01.Models;
+
+public class Order
+{
+    public int OrderId { get; set; }
+    
+    public int CustomerId { get; set; }
+    
+    public DateTime DateCreated { get; set; }
+    
+    public DateTime DateFulfilled { get; set; }
+    
+    public decimal Total { get; set; }
+    
+    public decimal Taxes { get; set; }
+}
